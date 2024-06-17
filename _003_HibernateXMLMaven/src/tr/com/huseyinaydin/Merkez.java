7fc29fcb5bca2c8f4210f39b5fbf98f38b621e2c
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-06-17 19:27:15 +0300

    Hibernate XML Configuration ve Maven.

---
+++ _003_HibernateXMLMaven/src/tr/com/huseyinaydin/Merkez.java
@@ -0,0 +1,35 @@
+package tr.com.huseyinaydin;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import tr.com.huseyinaydin.util.HibernateUtil;
+
+//بسم الله الرحمن الرحيم
+/**
+*
+* @author Huseyin_Aydin
+* @since 1994
+* @category Spring Web MVC ve JPA Hibernate
+*
+*/
+
+public class Merkez {
+
+	public static void main(String[] args) {
+		Personel personel = new Personel();
+		//personel.setPersonelId(1);
+		personel.setPersonelAdi("Hüseyin");
+		personel.setPersonelSoyadi("Aydın");
+		personel.setPersonelEmail("huseyinaydin99@gmail.com");
+
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.save(personel);
+		session.getTransaction().commit();
+		session.close();
+		sessionFactory.close();
+	}
+}
---
+++ _003_HibernateXMLMaven/src/tr/com/huseyinaydin/util/HibernateUtil.java
@@ -0,0 +1,18 @@
+package tr.com.huseyinaydin.util;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+//بسم الله الرحمن الرحيم
+/**
+*
+* @author Huseyin_Aydin
+* @since 1994
+* @category Spring Web MVC ve JPA Hibernate
+*
+*/
+
+public class HibernateUtil {
+
+}
